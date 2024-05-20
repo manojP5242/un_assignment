67f775f164db877261f196baf33d7a0c6719af5d
manojP5242 <manoj.palkar5242@gmail.com>
2024-05-20 21:05:10 +0700

    Part II: Testing

--- Program.cs
+++ Program.cs
@@ -1,21 +1,18 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using UnAssignmentWebApp;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.Configure<CustomSection>((settings) =>
 {
     builder.Configuration.GetSection("BeatsConfigInfo").Bind(settings);
     settings.IsServerReady = true;
 });
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -31,3 +28,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
